21f24759ac900936b7c8e824fb1c98f18e065024
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2018-04-07 20:32:33 +0200

    Added a WPF example to the project Project called Tests renamed to FastColoredTextBox.WinForm_Example

---
+++ FastColoredTextBox_WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using FastColoredTextBoxNS;
+using FastColoredTextBoxNS.Models.Syntaxes;
+using System.Windows;
+using System.Windows.Forms.Integration;
+
+namespace FastColoredTextBox_WPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			// Create the interop host control.
+			var host = new WindowsFormsHost();
+
+			// Create the FastColoredTextBox control.
+			var fctb = new FastColoredTextBox
+			{
+				Language = new CSharpSyntax()
+			};
+
+			// Assign the FastColoredTextBox control as the host control's child.
+			host.Child = fctb;
+
+			// Add the interop host control to the Grid
+			// control's collection of child controls.
+			this.ColoredTextBoxTutorial.Children.Add(host);
+		}
+	}
+}
