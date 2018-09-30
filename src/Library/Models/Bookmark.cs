39acecb9691684e0907478bf3cf3be5f6820541b
Tom PoLáKoSz <polakosz@freemail.hu>
2018-09-30 18:19:00 +0200

    Moves Bookmark class to the Models subfolder

---
+++ src/Library/Models/Bookmark.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace FastColoredTextBoxNS
+{
+    /// <summary>
+    /// Bookmark of FastColoredTextbox
+    /// </summary>
+    public class Bookmark
+    {
+        public FastColoredTextBox TB { get; private set; }
+
+        public string Name { get; set; }
+
+        public int LineIndex {get; set; }
+
+        /// <summary>
+        /// Color of bookmark sign
+        /// </summary>
+        public Color Color { get; set; }
+
+
+
+        public Bookmark(FastColoredTextBox tb, string name, int lineIndex)
+        {
+            this.TB = tb;
+            this.Name = name;
+            this.LineIndex = lineIndex;
+            Color = tb.BookmarkColor;
+        }
+
+
+
+        /// <summary>
+        /// Scroll textbox to the bookmark
+        /// </summary>
+        public virtual void DoVisible()
+        {
+            TB.Selection.Start = new Place(0, LineIndex);
+            TB.DoRangeVisible(TB.Selection, true);
+            TB.Invalidate();
+        }
+
+
+        public virtual void Paint(Graphics gr, Rectangle lineRect)
+        {
+            var size = TB.CharHeight - 1;
+            using (var brush = new LinearGradientBrush(new Rectangle(0, lineRect.Top, size, size), Color.White, Color, 45))
+                gr.FillEllipse(brush, 0, lineRect.Top, size, size);
+            using (var pen = new Pen(Color))
+                gr.DrawEllipse(pen, 0, lineRect.Top, size, size);
+        }
+    }
+}
